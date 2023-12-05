977dd48326101fc0f6466fd40864f5143593754b
Hamza Rahali <hamzarahali8@gmail.com>
2023-12-05 21:49:35 +0100

    Initial version

---
+++ src/main/java/traffic/trafficrestsoapapi/TrafficRestSoapApiApplication.java
@@ -0,0 +1,13 @@
+package traffic.trafficrestsoapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficRestSoapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficRestSoapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/traffic/trafficrestsoapapi/TrafficRestSoapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package traffic.trafficrestsoapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficRestSoapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
