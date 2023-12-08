912e38981c2d5c44afc31453c72d68c99691fe34
anisn <nouurianis@gmail.com>
2023-12-08 23:09:38 +0100

    cors fix

--- src/main/java/traffic/trafficrestsoapapi/controller/NotificationController.java
+++ src/main/java/traffic/trafficrestsoapapi/controller/NotificationController.java
@@ -3,10 +3,7 @@ package traffic.trafficrestsoapapi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import traffic.trafficrestsoapapi.entity.Notification;
 import traffic.trafficrestsoapapi.entity.User;
 import traffic.trafficrestsoapapi.service.NotificationService;
@@ -16,6 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/notifications")
 public class NotificationController {
 
